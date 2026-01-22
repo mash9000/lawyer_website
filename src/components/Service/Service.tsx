2ef3beffafee10963e6c2e5cd22d2a5dbb040c5b
simmetr <vm.certo@gmail.com>
2026-01-23 00:09:21 +0700

    style: Service стилизован

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,4 +1,9 @@
 import './service/service.scss';
+import './service/service--hover.scss';
+import './service/__icon/service__icon.scss';
+import './service/__heading/service__heading.scss';
+import './service/__description/service__description.scss';
+
 import type {ServiceProps} from "./model/ServiceProps.ts";
 import type {FC} from "react";
 
@@ -7,7 +12,8 @@ export const Service: FC<ServiceProps> = ({
                                               serviceDescription
                                           }) => {
     return (
-        <article className="service">
+        <article className="service service--hover">
+            <div className='service__icon'></div>
             <h3 className='service__heading'>{serviceName}</h3>
             <p className='service__description'>{serviceDescription}</p>
         </article>
