8ea43b1e59a7d85d0483f804c234b8a8845b0073
simmetr <vm.certo@gmail.com>
2026-01-23 15:09:12 +0700

    style: Service нормально адаптивен и добавлены уголки

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -1,4 +1,9 @@
 import './service/service.scss';
+import './service/__angles/service__angles.scss';
+import './service/__angles/service__angles--right-bottom.scss';
+import './service/__angles/service__angles--right-top.scss';
+import './service/__angles/service__angles--left-bottom.scss';
+import './service/__angles/service__angles--left-top.scss';
 import './service/service--hover.scss';
 import './service/__icon/service__icon.scss';
 import './service/__heading/service__heading.scss';
@@ -13,6 +18,10 @@ export const Service: FC<ServiceProps> = ({
                                           }) => {
     return (
         <article className="service service--hover">
+            <span className='service__angles service__angles--right-bottom'></span>
+            <span className='service__angles service__angles--right-top'></span>
+            <span className='service__angles service__angles--left-bottom'></span>
+            <span className='service__angles service__angles--left-top'></span>
             <div className='service__icon'></div>
             <h3 className='service__heading'>{serviceName}</h3>
             <p className='service__description'>{serviceDescription}</p>
