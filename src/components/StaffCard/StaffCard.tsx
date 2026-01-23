d4c56cea979beec4bb48008af4b32775104e1819
simmetr <vm.certo@gmail.com>
2026-01-23 14:13:55 +0700

    style: StaffCard анимирован

--- src/components/StaffCard/StaffCard.tsx
+++ src/components/StaffCard/StaffCard.tsx
@@ -1,6 +1,8 @@
 import './staff-card/staff-card.scss';
+import './staff-card/staff-card--hover.scss';
 import './staff-card/__container-for-avatar/staff-card__container-for-avatar.scss';
 import './staff-card/__container-for-avatar/__avatar/staff-card__container-for-avatar__avatar.scss';
+import './staff-card/__txt-info/staff-card__txt-info.scss';
 import './staff-card/__full-name/staff-card__full-name.scss';
 import './staff-card/__job-title/staff-card__job-title.scss';
 import './staff-card/__appeal/staff-card__appeal.scss';
@@ -17,17 +19,19 @@ export const StaffCard: FC<StaffCardProps> = ({
                                                   achievement
                                               }) => {
     return (
-        <div className="staff-card">
+        <div className="staff-card staff-card--hover">
             <div className='staff-card__container-for-avatar'>
                 <img
                     className='staff-card__container-for-avatar__avatar'
                     src={imageSrc}
                     alt={`фото сотрудника ${fullName.lastName} ${fullName.firstName} ${fullName.middleName}`}/>
             </div>
-            <p className='staff-card__full-name'>{`${fullName.lastName} ${fullName.firstName} ${fullName.middleName}`}</p>
-            <p className='staff-card__job-title'>{jobTitle}</p>
-            <p className='staff-card__appeal'>{appeal}</p>
-            <p className='staff-card__achievement'>{achievement}</p>
+            <div className='staff-card__txt-info'>
+                <p className='staff-card__full-name'>{`${fullName.lastName} ${fullName.firstName} ${fullName.middleName}`}</p>
+                <p className='staff-card__job-title'>{jobTitle}</p>
+                <p className='staff-card__appeal'>{appeal}</p>
+                <p className='staff-card__achievement'>{achievement}</p>
+            </div>
         </div>
     );
 }
