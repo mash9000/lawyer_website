d3cc0d5809fe0f7ddf5c4a470b1be1d801790bab
simmetr <vm.certo@gmail.com>
2026-01-23 22:38:53 +0700

    style: внутренние блоки созданы и выровнены

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import './header/header.scss';
 import './header/__nav/header__nav.scss';
 import './header/__nav/__communication-unit/header__nav__communication-unit.scss';
+import './header/__nav/__communication-unit/__inner-block/header__nav__communication-unit__inner-block.scss';
 import './header/__nav/__base-links/header__nav__base-links.scss';
+import './header/__nav/__base-links/__inner-block/header__nav__base-links__inner-block.scss';
+
 import type {HeaderProps} from "./model/HeaderProps.ts";
 import type {FC} from "react";
 
@@ -16,26 +19,31 @@ export const Header: FC<HeaderProps> = ({
         <header className='header'>
             <nav className='header__nav'>
                 <div className='header__nav__communication-unit'>
-                    <a className='header__nav__communication-unit--telephoneNumber'
-                       href={`tel:${contactTelephoneNumber}`}>{contactTelephoneNumber}</a>
-                    <a className='header__nav__communication-unit--contactEmail'
-                       href={`mailto:${contactEmail}`}>{contactEmail}</a>
-                    <button type='button' className=''
-                            onClick={() => requestACall.actionBeingPerformed()}>{requestACall.text}</button>
+                    <div
+                        className='header__nav__communication-unit__inner-block'>
+                        <a className='header__nav__communication-unit--telephoneNumber'
+                           href={`tel:${contactTelephoneNumber}`}>{contactTelephoneNumber}</a>
+                        <a className='header__nav__communication-unit--contactEmail'
+                           href={`mailto:${contactEmail}`}>{contactEmail}</a>
+                        <button type='button' className=''
+                                onClick={() => requestACall.actionBeingPerformed()}>{requestACall.text}</button>
+                    </div>
                 </div>
                 <div className='header__nav__base-links'>
-                    <a className='header__nav__base-lins--logo'
-                       href='http://localhost:5173/'
-                       target="_self">
-                        <h6>правозащитник</h6>
-                        <span>юридическая компания</span>
-                    </a>
-                    {baseLinks.map((link: { text: string, link: URL }) => (
-                        <a key={link.text}
-                           href={link.link.href}
-                           className='header__nav__base-lins--nav header__nav__base-lins--nav--hover'
-                           target='_blank'>{link.text}</a>
-                    ))}
+                    <div className='header__nav__base-links__inner-block'>
+                        <a className='header__nav__base-lins--logo'
+                           href='http://localhost:5173/'
+                           target="_self">
+                            <h6>правозащитник</h6>
+                            <span>юридическая компания</span>
+                        </a>
+                        {baseLinks.map((link: { text: string, link: URL }) => (
+                            <a key={link.text}
+                               href={link.link.href}
+                               className='header__nav__base-lins--nav header__nav__base-lins--nav--hover'
+                               target='_blank'>{link.text}</a>
+                        ))}
+                    </div>
                 </div>
             </nav>
             <div className='header__main-information'>
