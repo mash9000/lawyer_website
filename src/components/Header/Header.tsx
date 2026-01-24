cf5978230461774c8ffd1560c846dfe1a46bf307
simmetr <vm.certo@gmail.com>
2026-01-24 11:07:45 +0700

    styles: подправил bem-структуру

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import './header/header.scss';
 import './header/__nav/header__nav.scss';
 import './header/__nav/__communication-unit/header__nav__communication-unit.scss';
-import './header/__nav/__communication-unit/header__nav__communication-unit--requestACall.scss';
-import './header/__nav/__communication-unit/header__nav__communication-unit--telephoneNumber.scss';
-import './header/__nav/__communication-unit/header__nav__communication-unit--contactEmail.scss';
+import './header/__nav/__communication-unit/__inner-block/__request-a-call/header__nav__communication-unit__inner-block__request-a-call.scss';
+import './header/__nav/__communication-unit/__inner-block/__telephone-number/header__nav__communication-unit__inner-block__telephone-number.scss';
+import './header/__nav/__communication-unit/__inner-block/__contact-email/header__nav__communication-unit__inner-block__contact-email.scss';
 import './header/__nav/__communication-unit/__inner-block/header__nav__communication-unit__inner-block.scss';
 import './header/__nav/__base-links/header__nav__base-links.scss';
 import './header/__nav/__base-links/__inner-block/header__nav__base-links__inner-block.scss';
-import './header/__nav/__base-links/__inner-block/header__nav__base-links--logo.scss';
-import './header/__nav/__base-links/__inner-block/header__nav__base-links--nav.scss';
-import './header/__nav/__base-links/__inner-block/header__nav__base-links--nav--hover.scss';
-import './header/__nav/__base-links/__inner-block/header__nav__base-links--burger-menu.scss';
+import './header/__nav/__base-links/__inner-block/__logo/header__nav__base-links__inner-block__logo.scss';
+import './header/__nav/__base-links/__inner-block/__link/header__nav__base-links__link.scss';
+import './header/__nav/__base-links/__inner-block/__link/header__nav__base-links__link--hover.scss';
+import './header/__nav/__base-links/__inner-block/__burger-menu/header__nav__base-links__inner-block__burger-menu.scss';
 
 import './custom-style/custom-style.scss';
 import './custom-style/__style-one/custom-style__style-one.scss';
@@ -32,18 +32,18 @@ export const Header: FC<HeaderProps> = ({
             <nav className='header__nav'>
                 <div className='header__nav__communication-unit'>
                     <div className='header__nav__communication-unit__inner-block'>
-                        <a className='header__nav__communication-unit--telephoneNumber'
+                        <a className='header__nav__communication-unit__inner-block__telephone-number'
                            href={`tel:${contactTelephoneNumber}`}>{contactTelephoneNumber}</a>
-                        <a className='header__nav__communication-unit--contactEmail'
+                        <a className='header__nav__communication-unit__inner-block__contact-email'
                            href={`mailto:${contactEmail}`}>{contactEmail}</a>
                         <button type='button'
-                                className='header__nav__communication-unit--requestACall'
+                                className='header__nav__communication-unit__inner-block__request-a-call'
                                 onClick={() => requestACall.actionBeingPerformed()}>{requestACall.text}</button>
                     </div>
                 </div>
                 <div className='header__nav__base-links'>
                     <div className='header__nav__base-links__inner-block'>
-                        <a className='header__nav__base-links--logo'
+                        <a className='header__nav__base-links__inner-block__logo'
                            href='http://localhost:5173/'
                            target="_self">
                             <h6>правозащитник</h6>
@@ -52,10 +52,10 @@ export const Header: FC<HeaderProps> = ({
                         {baseLinks.map((link: { text: string, link: URL }) => (
                             <a key={link.text}
                                href={link.link.href}
-                               className='header__nav__base-links--nav header__nav__base-links--nav--hover'
+                               className='header__nav__base-links__link header__nav__base-links__link--hover'
                                target='_blank'>{link.text}</a>
                         ))}
-                        <div className='header__nav__base-links--burger-menu'>
+                        <div className='header__nav__base-links__inner-block__burger-menu'>
                             <div></div>
                             <div></div>
                             <div></div>
