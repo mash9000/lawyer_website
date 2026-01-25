6743cd78090f6deb6b7207a0a01845a942ff650f
simmetr <vm.certo@gmail.com>
2026-01-25 17:54:53 +0700

    refactor: изменил наименование ссылки в nav

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,11 +1,10 @@
 export const baseLinks = [
     {
-        text: 'Услуги',
+        text: 'Прайс',
         link: new URL('http://localhost:5173/')
     },
     {
-        text: 'О этой компании',
-        link: new URL('http://localhost:5173/')
+        text: 'О этой компании',        link: new URL('http://localhost:5173/')
     },
     {
         text: 'Наши юристы',
